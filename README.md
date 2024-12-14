09c3bb978c4fb4ddba35bc9c32e5f3e133b19dfd
eliminator <eli.entrich@gmail.com>
2024-12-14 14:03:19 +0100

    setup test project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RpiZeroWebApi
 dotnet Web Api for Raspberry pi Zero 2Wg
+## Installation
+
+use installscript from https://www.petecodes.co.uk/install-and-use-microsoft-dot-net-8-with-the-raspberry-pi/
+wget -O - https://raw.githubusercontent.com/pjgpetecodes/dotnet8pi/main/install.sh | sudo bash
+
+reboot
+### Install package
+et add package System.Device.Gpio
+
+## Examples
+example to control pins in Program.cs
+
+more projects on https://github.com/pjgpetecodes/dotnet8pi
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Device.Gpio;
+
+Console.WriteLine("Hello World!");
+
+GpioController controller = new GpioController(PinNumberingScheme.Board);
+var pin = 10;
+var buttonPin = 26;
+
+controller.OpenPin(pin, PinMode.Output);
+controller.OpenPin(buttonPin, PinMode.InputPullUp);            
+
+try
+{
+    while (true)
+    {
+        if (controller.Read(buttonPin) == false)
+        {
+            controller.Write(pin, PinValue.High);
+        }
+        else
+        {
+            controller.Write(pin, PinValue.Low);
+        }
+    }
+}
+finally
+{
+    controller.ClosePin(pin);
+    controller.ClosePin(buttonPin);
+}
